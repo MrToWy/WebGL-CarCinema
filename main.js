1847a77bed5a9c3a2c5f1542d0ce41f0b7c62052
Sophie Fürhoff <sophie.fuerhoff@stud.hs-hannover.de>
2022-06-30 11:20:35 +0200

    transparente Scheiben 2.0

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const input = document.getElementById("input")
 let tolerance = 0.01;
 let updateId;
 let previousDelta = 0;
-let fpsLimit = 80;
+let fpsLimit = 15;
 const targetTextureWidth = 1024;
 const targetTextureHeight = targetTextureWidth;
 let camRotation = -45;
@@ -393,47 +393,58 @@ async function init() {
         const position = [0,-0.5,0.0];
         const eye = [0,0,2];
         const carCamRotation = new Rotation(-90, 0, camRotation);
+
         gl.clearColor(0., 1., 0., 1.);
+        gl.depthMask(true);
+        gl.disable(gl.BLEND);
 
         // Inside
         const carInsidePosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const car = new DrawableObject(carProgram, null, carInsidePosition, carInsideVertices, null, true)
+        const car = new DrawableObject(carProgram, null, carInsidePosition, carPath + "car_inside.obj", carInsideVertices, null, true)
         await car.draw()
+
+        // Airing
+        const carAiringPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
+        const carAiring = new DrawableObject(carProgram, null, carAiringPosition, carPath + "car_airing.obj", carAiringVertices, null, false)
+        await carAiring.draw()
 
         // Door Right Front
         const carDoorRightFrontPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carDoorRightFront = new DrawableObject(carProgram, null, carDoorRightFrontPosition, carDoorRightFrontVertices, null, false)
+        const carDoorRightFront = new DrawableObject(carProgram, null, carDoorRightFrontPosition, carPath + "car_door_right_front.obj", carDoorRightFrontVertices, null, false)
         await carDoorRightFront.draw()
 
         // Door Left Front
         const carDoorLeftFrontPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carDoorLeftFront = new DrawableObject(carProgram, null, carDoorLeftFrontPosition, carDoorLeftFrontVertices, null, false)
+        const carDoorLeftFront = new DrawableObject(carProgram, null, carDoorLeftFrontPosition, carPath + "car_door_left_front.obj", carDoorLeftFrontVertices, null, false)
         await carDoorLeftFront.draw()
+
+        // Rear Mirror
+        const carRearMirrorPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
+        const carRearMirror = new DrawableObject(carProgram, null, carRearMirrorPosition, carPath + "car_rear_mirror.obj", carRearMirrorVertices, null, false)
+        await carRearMirror.draw()
+
+
+        gl.depthMask(false);
+        gl.blendColor(0.0,0.0,0.0,0.5);
+        gl.blendEquationSeparate(gl.FUNC_ADD,gl.FUNC_ADD);
+        gl.blendFuncSeparate(gl.SRC_ALPHA,gl.CONSTANT_ALPHA,gl.CONSTANT_ALPHA,gl.CONSTANT_ALPHA);
+        gl.enable(gl.BLEND);
+
+        // Windscreen
+        const carWindscreenPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
+        const carWindscreen = new DrawableObject(carProgram, null, carWindscreenPosition, carPath + "car_windscreen.obj", carWindscreenVertices, null, false)
+        await carWindscreen.draw()
 
         // Door Window Left Front
         const carDoorWindowLeftFrontPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carDoorWindowLeftFront = new DrawableObject(carProgram, null, carDoorWindowLeftFrontPosition, carDoorWindowLeftFrontVertices, null, false)
+        const carDoorWindowLeftFront = new DrawableObject(carProgram, null, carDoorWindowLeftFrontPosition, carPath + "car_door_window_left_front.obj", carDoorWindowLeftFrontVertices, null, false)
         await carDoorWindowLeftFront.draw()
 
         // Door Window Right Front
         const carDoorWindowRightFrontPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carDoorWindowRightFront = new DrawableObject(carProgram, null, carDoorWindowRightFrontPosition, carDoorWindowRightFrontVertices, null, false)
+        const carDoorWindowRightFront = new DrawableObject(carProgram, null, carDoorWindowRightFrontPosition, carPath + "car_door_window_right_front.obj", carDoorWindowRightFrontVertices, null, false)
         await carDoorWindowRightFront.draw()
 
-        // Windscreen
-        const carWindscreenPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carWindscreen = new DrawableObject(carProgram, null, carWindscreenPosition, carWindscreenVertices, null, false)
-        await carWindscreen.draw()
-
-        // Rear Mirror
-        const carRearMirrorPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carRearMirror = new DrawableObject(carProgram, null, carRearMirrorPosition, carRearMirrorVertices, null, false)
-        await carRearMirror.draw()
-
-        // Airing
-        const carAiringPosition = new Position(carCamRotation, null, position, [scaleFactorCar, scaleFactorCar, scaleFactorCar], eye)
-        const carAiring = new DrawableObject(carProgram, null, carAiringPosition, carAiringVertices, null, false)
-        await carAiring.draw()
     }
 
     requestAnimationFrame(loop);
