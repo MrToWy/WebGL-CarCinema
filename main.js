757d1be2dabc553f2fd8ee9884a45a4d0f2d29e1
Sophie Fürhoff <sophie.fuerhoff@stud.hs-hannover.de>
2022-07-04 00:19:50 +0200

    delete mipmap

--- main.js
+++ main.js
@@ -170,7 +170,6 @@ async function init() {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE );
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE );
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, textureImage)
-        //gl.generateMipmap(gl.TEXTURE_2D)
         gl.bindTexture(gl.TEXTURE_2D, null)
 
         // movie
