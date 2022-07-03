1ff11314e65ef146541b2e10c99f06e58e97a509
Sophie Fürhoff <sophie.fuerhoff@stud.hs-hannover.de>
2022-07-04 00:19:17 +0200

    video texture

--- main.js
+++ main.js
@@ -159,10 +159,16 @@ async function init() {
         await carRearMirror.draw();
 
 
+        gl.useProgram(movieProgram);
+        var textureLocation2 = gl.getUniformLocation(movieProgram, "texture");
+        gl.uniform1i(textureLocation2, 0);
 
-
-
+        gl.useProgram(movieProgram);
+        gl.activeTexture(gl.TEXTURE0);
         gl.bindTexture(gl.TEXTURE_2D, movieTexture)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE );
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE );
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, textureImage)
         //gl.generateMipmap(gl.TEXTURE_2D)
         gl.bindTexture(gl.TEXTURE_2D, null)
