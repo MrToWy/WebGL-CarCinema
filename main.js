edda8ac32e6f8bd828b91d4a09844c21960ff319
Sophie Fürhoff <sophie.fuerhoff@stud.hs-hannover.de>
2022-07-04 21:51:48 +0200

    screen

--- main.js
+++ main.js
@@ -165,11 +165,10 @@ async function init() {
 
         gl.useProgram(movieProgram);
         gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, movieTexture)
+        gl.bindTexture(gl.TEXTURE_2D, movieTexture);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S,gl.CLAMP_TO_EDGE );
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T,gl.CLAMP_TO_EDGE );
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, textureImage)
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureImage)
         gl.bindTexture(gl.TEXTURE_2D, null)
 
         // movie
@@ -180,12 +179,11 @@ async function init() {
         const movie = new DrawableObject(movieProgram, moviePosition,  movieVertices, false )
         movie.setTexture(movieTexture);
         await movie.draw();
-/*
+
         const structurePosition = new Position(movieRotation, [-7.0,5.0,-60.],  [movieScaleFactor, movieScaleFactor, movieScaleFactor], eye, look)
         const structure = new DrawableObject(movieProgram, structurePosition,  structureVertices, false )
         await structure.draw();
 
- */
 
 
         // draw transperent objects
